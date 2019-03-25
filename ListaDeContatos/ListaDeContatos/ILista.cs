6a78cbc8a5689eb12f369a9eb554b5b6ebf930fd
Douglas Tertuliano <douglasjtds@gmail.com>
2019-03-24 21:02:28 -0300

    adicionando interface lista e classe ListaDeContatos

---
+++ ListaDeContatos/ListaDeContatos/ILista.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDeContatos
+{
+    public interface ILista
+    {
+        void Pesquisa(Object chave);
+        void Insere(Object x);
+        Object Retira(Object chave);
+        Object RetiraPrimeiro();
+        Object Primeiro();
+        bool IsListaVazia();
+    }
+}
---
+++ ListaDeContatos/ListaDeContatos/ListaDeContatos.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDeContatos
+{
+    public class ListaDeContatos : ILista
+    {
+        #region Métodos herdados da interface base
+        public void Pesquisa(object chave)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(object x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Retira(object chave)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object RetiraPrimeiro()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Primeiro()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsListaVazia()
+        {
+            throw new NotImplementedException();
+        } 
+        #endregion
+
+        #region Outros métodos que não estão na interface
+        //completando com outros métodos 
+        public object RetiraUltimo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Ultimo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object TamanhoLista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Insere(Object x, Object chave)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Concatenar(Object[] itens)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Copia(Object item)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
