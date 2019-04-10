dd729c15ac1882eb12e2799d421b921cf2de3d8b
Douglas Tertuliano <douglasjtds@gmail.com>
2019-04-09 23:19:24 -0300

    continuando pilha encadeada

--- ListaDeContatos/ListaDeContatos/PilhaEncadeada.cs
+++ ListaDeContatos/ListaDeContatos/PilhaEncadeada.cs
@@ -8,21 +8,52 @@ namespace ListaDeContatos
 {
     public class PilhaEncadeada : IPilhaEncadeada
     {
-        #region Métodos herdados da interface base
-        public object Desempilha()
+        public class Celula
         {
-            throw new NotImplementedException();
+            internal Object item;
+            internal Celula prox;
+        }
+        public Celula topo;
+        private int tam;
+
+        public void CriaListaVazia()
+        {
+            topo = null;
+            tam = 0;
         }
 
+
+        #region Métodos herdados da interface base
         public void Empilha(object x)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            Celula aux = topo;
+            topo = new Celula();
+            topo.item = x;
+            topo.prox = aux;
+            tam++;
+        }
+
+        public object Desempilha()
+        {
+            //throw new NotImplementedException();
+            if (IsPilhaVazia())
+                throw new Exception("Erro: A pilha está vazia.");
+            Object item = topo.item;
+            tam--;
+            return item;
         }
 
         public bool IsPilhaVazia()
         {
-            throw new NotImplementedException();
-        } 
+            //throw new NotImplementedException();
+            return (topo == null);
+        }
         #endregion
+
+        public int Tamanho()
+        {
+            return tam;
+        }
     }
 }
